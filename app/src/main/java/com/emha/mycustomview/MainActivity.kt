7cf954cb70b0169865002fcc6d76571139968457
Syafiq Fajrian Emha <syafiqfajrianemha@gmail.com>
2023-09-12 14:51:39 +0700

    first commit

---
+++ app/src/main/java/com/emha/mycustomview/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.emha.mycustomview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Toast
+import com.emha.mycustomview.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setMyButtonEnable()
+
+        binding.myEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                // Do nothing.
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                setMyButtonEnable()
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                // Do nothing.
+            }
+        })
+
+        binding.myButton.setOnClickListener {
+            Toast.makeText(this@MainActivity, binding.myEditText.text, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun setMyButtonEnable() {
+        val result = binding.myEditText.text
+        binding.myButton.isEnabled = result != null && result.toString().trim().isNotEmpty()
+    }
+}
